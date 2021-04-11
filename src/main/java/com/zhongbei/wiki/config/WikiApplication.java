946985897c3ad9bf138d0b32986e108cab6208ec
liubing0806 <liubing0806@qq.com>
2021-04-11 10:41:18 +0800

    集成mybatis mybatis——generator

--- src/main/java/com/zhongbei/wiki/config/WikiApplication.java
+++ src/main/java/com/zhongbei/wiki/config/WikiApplication.java
@@ -1,5 +1,6 @@
 package com.zhongbei.wiki.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@MapperScan("com.zhongbei.wiki.mapper")
 public class WikiApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
